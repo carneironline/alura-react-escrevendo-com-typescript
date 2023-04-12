7c2e08662938f04738d787028f410203dab0c137
Rodrigo Carneiro <carneironline@gmail.com>
2023-04-12 01:03:43 -0300

    usando css modules com sass

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import style from './Lista.module.scss'
+
 export default function Lista() {
     const tarefas = [
         {tarefa: 'React', tempo: '02:00:00'},
@@ -8,12 +10,12 @@ export default function Lista() {
     ]
 
     return (
-        <aside>
+        <aside className={style.listaTarefas}>
             <h2>Estudos do dia</h2>
 
             <ul>
                 {tarefas.map((item, index) => (
-                    <li key={index}>
+                    <li key={index} className={style.item}>
                         <h3>{item.tarefa}</h3>
                         <span>{item.tempo}</span>
                     </li>
